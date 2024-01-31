14cae4c3a51e81ecade7a8d76b2b9390660f8495
AlessandroTedde <148778362+AlessandroTedde@users.noreply.github.com>
2024-01-31 16:04:01 +0100

    Aggiungere i file di progetto.

---
+++ EsercizioContoCorrenteBED3/ContoCorrente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Resolvers;
+
+namespace EsercizioContoCorrenteBED3
+{
+    internal class ContoCorrente
+    {
+        public bool IsContoAperto { get; set; }
+        public int Saldo  { get; set; }
+        public uint Prel { get; set; }
+        public uint Vers  { get; set; }
+        
+        
+        
+        public void Prelievo()
+        {
+            
+            if (IsContoAperto == true && Prel < Saldo)
+            {
+               Console.WriteLine(Saldo - Prel);
+            }
+            if (IsContoAperto == false)
+            {
+               Console.WriteLine("Non hai ancora aperto un conto corrente.");
+            }
+            if (Prel > Saldo) 
+            {
+                Console.WriteLine("Non hai abbastanza liquidità per prelevare questa somma.");
+            }
+        }        
+        public void Versamento()
+        {
+            if (IsContoAperto == true)
+            {
+               Console.WriteLine(Saldo + Vers);
+            }
+            if (IsContoAperto == false)
+            {
+               Console.WriteLine("Non hai ancora aperto un conto corrente.");
+            }
+
+        }
+        public ContoCorrente(bool IsContoAperto, int Saldo, uint Prel, uint Vers)
+        {
+            this.Saldo = Saldo;
+            this.Prel = Prel;
+            this.Vers = Vers;
+            this.IsContoAperto = IsContoAperto;
+        }
+    }
+}
---
+++ EsercizioContoCorrenteBED3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioContoCorrenteBED3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContoCorrente conto1 = new ContoCorrente();
+            Console.WriteLine("Salve! Che cosa vorresti fare oggi in banca?" + "\n");
+            Console.WriteLine("Scegli un'opzione:");
+            Console.WriteLine("1) Apri un conto corrente");
+            Console.WriteLine("2) Versamento");
+            Console.WriteLine("3) Prelievo");
+            Console.WriteLine("Seleziona un'opzione: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    
+            }
+
+        }
+    }
+}
